3e09217a56d15542b01fa91f95ad49daef6ed1a3
piradiusquared <kpan5121@gmail.com>
2025-10-08 11:49:19 +1000

    Added dataset loading. Columns are as expected (in modules for now)

--- recognition/FLAN_s4885380/modules.py
+++ recognition/FLAN_s4885380/modules.py
@@ -1,2 +1,15 @@
 import torch
-import torch.nn as nn
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Device:", device)
+
+
+# Testing for dataset loading
+from datasets import load_dataset
+from transformers import AutoTokenizer
+
+dataset = load_dataset("BioLaySumm/BioLaySumm2025-LaymanRRG-opensource-track")
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+
+
+print(dataset["train"].column_names)
