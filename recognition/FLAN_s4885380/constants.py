4dc7fe89e6ebd23337fd067443b3e9a4b645aca9
piradiusquared <kpan5121@gmail.com>
2025-10-23 22:36:47 +1000

    Created helper file constants.py. Contains all values which are used for argument purposes.

---
+++ recognition/FLAN_s4885380/constants.py
@@ -0,0 +1,23 @@
+
+MODEL_NAME = "google/flan-t5-base"
+
+TRAIN_FILE = "hf://datasets/BioLaySumm/BioLaySumm2025-LaymanRRG-opensource-track/data/train-00000-of-00001.parquet"
+VALIDATION_FILE = "hf://datasets/BioLaySumm/BioLaySumm2025-LaymanRRG-opensource-track/data/validation-00000-of-00001.parquet"
+TEST_FILE = "hf://datasets/BioLaySumm/BioLaySumm2025-LaymanRRG-opensource-track/data/test-00000-of-00001.parquet"
+
+INPUT_COLUMN = "radiology_report"
+TARGET_COLUMN = "layman_report"
+
+EPOCHS = 3
+LEARNING_RATE = 1e-4
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 16
+MAX_INPUT_LENGTH = 1024
+MAX_TARGET_LENGTH = 512
+
+LORA_R = 16
+LORA_ALPHA = 32
+LORA_DROPOUT = 0.05
+LORA_TARGET_MODULES = ["q", "v"]
+
+OUTPUT_DIR = "t5-base-lora-tuned"
