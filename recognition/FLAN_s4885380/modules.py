a966c5f2a6f333a8077b9f1d2400bf46d1c03bf4
piradiusquared <kpan5121@gmail.com>
2025-10-26 11:22:56 +1000

    Load LoRA into modules.py. Test with some optimal LoRA configuration values for Seq2Seq training. Optimiser used is AdamW, as used before in demos.

---
+++ recognition/FLAN_s4885380/modules.py
@@ -0,0 +1,52 @@
+from typing import Tuple
+import torch
+import pandas as pd
+import numpy as np
+import evaluate
+from tqdm.auto import tqdm
+
+from torch.utils.data import Dataset, DataLoader
+from torch.optim import AdamW
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSeq2SeqLM,
+    get_scheduler
+)
+from peft import LoraConfig, get_peft_model, TaskType
+
+from dataset import *
+from constants import *
+
+class FlanModel:
+    def __init__(self):
+        pass
+
+    def build(self) -> Tuple[AutoModelForSeq2SeqLM, AutoTokenizer]:
+        # Load actual Flan-T5 models
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
+
+        # Load LoRA
+        lora_config = LoraConfig(
+            r=LORA_R,
+            lora_alpha=LORA_ALPHA,
+            target_modules=LORA_TARGET_MODULES,
+            lora_dropout=LORA_DROPOUT,
+            bias="none",
+            task_type=TaskType.SEQ_2_SEQ_LM
+        )
+
+        model = get_peft_model(model, lora_config)
+        model.print_trainable_parameters()
+        return model, tokenizer
+        # model.to(device)
+
+    def setup_optimiser(self, model, train_dataloader) -> Tuple[AdamW, get_scheduler]:
+        optimizer = AdamW(model.parameters(), lr=LEARNING_RATE)
+        num_training_steps = EPOCHS * len(train_dataloader)
+        lr_scheduler = get_scheduler(
+            "linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
+        )
+
+        return optimizer, lr_scheduler
+
