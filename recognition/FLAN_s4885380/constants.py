a49c3ae4229e8c78251624cd51d81064889b6014
piradiusquared <kpan5121@gmail.com>
2025-10-30 19:21:19 +1000

    Update constants to best performing values (LoRA parameters, training parameters)

--- recognition/FLAN_s4885380/constants.py
+++ recognition/FLAN_s4885380/constants.py
@@ -8,18 +8,20 @@ TEST_FILE = "hf://datasets/BioLaySumm/BioLaySumm2025-LaymanRRG-opensource-track/
 INPUT_COLUMN = "radiology_report"
 TARGET_COLUMN = "layman_report"
 
+MODEL_PROMPT = "translate this radiology report into a summary for a layperson: "
+
 TRAIN_SPLIT = 0.7
 VALIDATION_SPLIT = 0.3
 
 EPOCHS = 3
-LEARNING_RATE = 1e-4
-TRAIN_BATCH_SIZE = 8
-VALID_BATCH_SIZE = 16
-MAX_INPUT_LENGTH = 1024
-MAX_TARGET_LENGTH = 512
+LEARNING_RATE = 3e-4
+TRAIN_BATCH_SIZE = 64
+VALID_BATCH_SIZE = 128
+MAX_INPUT_LENGTH = 256
+MAX_TARGET_LENGTH = 128
 
-LORA_R = 16
-LORA_ALPHA = 32
+LORA_R = 32
+LORA_ALPHA = 64
 LORA_DROPOUT = 0.05
 LORA_TARGET_MODULES = ["q", "v"]
 
