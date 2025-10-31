e43907a36990579d809fe1d1f79bd1d944c6521f
Kevin Pan <kpan5121@gmail.com>
2025-10-31 13:50:00 +1000

    Enhance documentation with method docstrings
    Added docstrings to methods for better documentation.

--- recognition/FLAN_s4885380/dataset.py
+++ recognition/FLAN_s4885380/dataset.py
@@ -5,6 +5,10 @@ import numpy as np
 from torch.utils.data import Dataset
 from constants import *
 
+"""
+Held-out data splitter for training and evaluation.
+Splits the data into 70/30 ratio
+"""
 class SplitData:
     def __init__(self, file_path: str, sample_size: int | None = None) -> None:
         self.dataframe = pd.read_parquet(file_path)
@@ -13,6 +17,9 @@ class SplitData:
         # else:
         #     self.dataframe = self.dataframe[100:300] # Testing split
 
+    """
+    Returns both splits at once from the original dataframe
+    """
     def get_splits(self) -> tuple[pd.DataFrame, pd.DataFrame]:
         split_index = np.random.random(len(self.dataframe)) < 0.7
         train = self.dataframe[split_index]
@@ -21,6 +28,9 @@ class SplitData:
         return train, validation
         
 
+"""
+Custom dataset loader and preprocessor. Prepends 1 of 4 similar prompts for training and evaluation.
+"""
 class FlanDataset(Dataset):
     def __init__(self, dataframe: pd.DataFrame, tokenizer) -> None:
         self.tokenizer = tokenizer
@@ -41,25 +51,28 @@ class FlanDataset(Dataset):
 
     def __len__(self) -> int:
         return len(self.dataframe)
-    
-    def __getitem__(self, index: int) -> list:
-        row = self.dataframe.iloc[index]
 
-        rand_prefix = random.choice(self._prompts)
+    """
+    Tokenises the inputs using the tokenizer API. Converts strings into NLP suitable tensors
+    """
+    def __getitem__(self, index: int) -> list:
+        row = self.dataframe.iloc[index] # Selects slices using iloc index
+
+        rand_prefix = random.choice(self._prompts) # Selects random prefix
         report = rand_prefix + str(row[INPUT_COLUMN])
         summary = str(row[TARGET_COLUMN])
 
-        model_inputs = self.tokenizer(
+        model_inputs = self.tokenizer( # Tokenises the radiology report
             report,
             max_length=MAX_INPUT_LENGTH,
             truncation=True
         )
 
-        with self.tokenizer.as_target_tokenizer():
+        with self.tokenizer.as_target_tokenizer(): # Tokenises the layman summary
             labels = self.tokenizer(
                 summary,
                 max_length=MAX_TARGET_LENGTH,
                 truncation=True
             )
-        model_inputs["labels"] = labels["input_ids"]
+        model_inputs["labels"] = labels["input_ids"] # Join report and summary together
         return model_inputs
